9d10fc14a2c9760fcae6a46cdcc70bd158fc4834
kathiriya18 <131762299+kathiriya18@users.noreply.github.com>
2023-05-18 15:41:40 +0530

    Add files via upload

---
+++ 3863 p42.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+main()
+{
+	int k,h;
+	
+	for(k=5;k>=1;k--)
+    {
+       for(h=k;h<=5;h++)
+	   {
+	   	 printf("%d",k);
+	
+	   }	
+        printf("\n");
+	}	
+	
+	
+	
+}
---
+++ 3863 p44.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+main()
+{
+	int k,h;
+	
+	for(k=1;k<=5;k++)
+    {
+    	
+    	
+
+		
+       for(h=1;h<=k;h++)
+	   {
+	   	 printf("%d",k);
+	
+	   }	
+        printf("\n");
+	}	
+	
+	
+	
+}
